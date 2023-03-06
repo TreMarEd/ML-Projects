133b3eb4951eb9750f7c953beda0991f7882286e
Marius Tresoldi <tremared@gmail.com>
2023-03-06 14:29:39 +0100

    improved console printing

--- task 0/main.py
+++ task 0/main.py
@@ -16,20 +16,20 @@ y_train = train.iloc[:, 1]
 
 lin_reg = linear_model.LinearRegression()
 lin_reg.fit(train.iloc[:, 2:], train.iloc[:, 1])
-print(lin_reg.coef_)
-print(lin_reg.intercept_)
+
+# As the correct predictor is the mean function the coefficients should all be 0.1 with intercept 0
+print("\nregression coefficients: \n", lin_reg.coef_)
+print("\nintercept \n", lin_reg.intercept_)
 
 rmse = mean_squared_error(y_train, lin_reg.predict(x_train))**0.5
-print(rmse)
-
 
 # MODEL PREDICTION AND OUTPUT
+
 # test data set without labels, predictions on this data set will be submitted for the project
 test = pd.read_csv("./task 0/Input/test.csv")
 
 # submission template, to be overwritten with my solution
 sample = pd.read_csv("./task 0/Input/sample.csv")
-sample["y"] = lin_reg.predict(test.iloc[:,1:])
+sample["y"] = lin_reg.predict(test.iloc[:, 1:])
 sample.to_csv("./task 0/result.csv", index=False)
 
-print("hoi")
