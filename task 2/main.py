6cf045ab34317d052be90bd8e92620b51f178df6
Marius Tresoldi <tremared@gmail.com>
2023-03-31 15:57:16 +0200

    went back to imputation only on features, best predictor is matern kernel

--- task 2/main.py
+++ task 2/main.py
@@ -11,9 +11,10 @@ from sklearn.gaussian_process.kernels import DotProduct, RBF, Matern, RationalQu
 from sklearn.metrics import r2_score
 from sklearn.model_selection import cross_val_score
 
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
 
-
-def data_loading(random_state=0, max_iter=10):
+def data_loading():
     """
     This function loads the training and test data, preprocesses it, removes the NaN values and interpolates the missing 
     data using imputation
@@ -26,6 +27,8 @@ def data_loading(random_state=0, max_iter=10):
     y_train: array of floats, training output with labels
     X_test: matrix of floats: dim = (100, ?), test input with features
     """
+
+    # TODO: visually check the correctness of all these transforamtions
     # Load training data
     train_df = pd.read_csv("./task 2/train.csv")
     
@@ -42,90 +45,61 @@ def data_loading(random_state=0, max_iter=10):
 
     train_df = train_df.replace(to_replace=seasons, value=temperatures)
     #original, non-imputed test data matrix 
-    test_df = test_df.replace(to_replace=seasons, value=temperatures)
+    X_test_orig = test_df.replace(to_replace=seasons, value=temperatures)
 
     # visual inspection of the data shows that no column has significantly different nullity compared to the others except
     # for season, which is always present. Hence, no column should be dropped completely. Visual inspection also shows that 
     # there are almost no records with complete information, such that discarding missing rows is not feasible
 
     # drop records with missing labels for regression training
-    #train_reg = train_df.dropna(subset=['price_CHF'])
-    #y_train = train_reg['price_CHF']
+    train_reg = train_df.dropna(subset=['price_CHF'])
+    y_train = train_reg['price_CHF']
+    # original, non-imputed training data matrix
+    X_train_orig = train_reg.drop(['price_CHF'], axis=1)
 
-    #imputate training data
-    imp_labels = IterativeImputer(max_iter=max_iter, random_state=random_state)
-    imp_labels.fit(train_df)
-    train_df = imp_labels.transform(train_df)
-    X_train = np.delete(train_df, 2, axis=1)
-    y_train = train_df[:,2]
+    # Use entire feature data set for multivariate imputation training
+    train_imp = train_df.drop(['price_CHF'], axis=1)
+    imp = IterativeImputer(max_iter=10)
+    imp.fit(train_imp)
 
-
-    # imputate test data
-    imp_labels = IterativeImputer(max_iter=max_iter, random_state=random_state)
-    imp_labels.fit(test_df)
-    X_test = imp_labels.transform(test_df)
-
+    # transform regression features
+    X_train = imp.transform(X_train_orig)
+    X_test = imp.transform(X_test_orig)
 
     assert (X_train.shape[1] == X_test.shape[1]) and (X_train.shape[0] == y_train.shape[0]) and (X_test.shape[0] == 100), "Invalid data shape"
     return X_train, y_train, X_test
 
-def modeling_and_prediction(X_train, y_train, X_test):
-    """
-    This function defines the model, fits training data and then does the prediction with the test data 
-
-    Parameters
-    ----------
-    X_train: matrix of floats, training input with 10 features
-    y_train: array of floats, training output
-    X_test: matrix of floats: dim = (100, ?), test input with 10 features
-
-    Returns
-    ----------
-    y_test: array of floats: dim = (100,), predictions on test set
-    """
-
-    y_pred=np.zeros(X_test.shape[0])
-    #TODO: Define the model and fit it using training data. Then, use test data to make predictions
-
-    assert y_pred.shape == (100,), "Invalid data shape"
-    return y_pred
-
 
 if __name__ == "__main__":
 
-    # use multiple imputation: m imputations with different RNG seeds
-    m = 1
+    X_train, y_train, X_test = data_loading()
 
-    # rational quadratic performs best
-    kernels = [DotProduct(sigma_0=0.1), DotProduct(sigma_0=1)]
-               #RBF(length_scale=0.01), RBF(length_scale=1), RBF(length_scale=10), 
-               #Matern(length_scale=0.01), Matern(length_scale=0.1), Matern(length_scale=1), 
-               #RationalQuadratic(length_scale=0.1, alpha=10), RationalQuadratic(length_scale=10, alpha =0.1), RationalQuadratic(length_scale=1, alpha=1)]
+    kernels = [DotProduct(sigma_0=0.1),
+               RBF(length_scale=0.1, length_scale_bounds=(1e-06, 100000.0)), 
+               Matern(length_scale=0.2, nu=0.2), Matern(length_scale=0.3, nu=0.3), Matern(length_scale=0.4, nu=0.4), Matern(length_scale=0.5, nu=0.5),
+               RationalQuadratic(length_scale=1, alpha=1)]
     
-    cv_scores = pd.DataFrame(index=[i for i in range(m)], columns=[str(k) for k in kernels])
+    cv_scores = pd.DataFrame(columns=["CV_score"], index=[str(k) for k in kernels])
 
-    for random_state in range(m):
-        for kernel in kernels:
-            print("\n using kernel ", str(kernel), "\n")
-            X_train, y_train, X_test = data_loading(random_state=random_state)
+    for kernel in kernels:
+        print("\nUsing kernel ", str(kernel), "\n")
+        gpr = GaussianProcessRegressor(kernel=kernel, alpha = 5e-9)
+        cv_scores.loc[str(kernel), "CV_score"] = np.mean(cross_val_score(gpr, X_train, y_train, cv=10, scoring="r2"))
 
-            gpr = GaussianProcessRegressor(kernel=kernel, alpha = 5e-8)
-            #gpr.fit(X_train, y_train)
-            cv_scores.loc[random_state, str(kernel)] = np.mean(cross_val_score(gpr, X_train, y_train, cv=10, scoring="r2"))
+    # dot product always with bad performance, RBF with terrbible performance for all parametes,
+    # matern and rational quadratic are competeteive, polynomial does not exist in the module. 
+    # Choose matern(0.3, 0.3) in the following
+    # TODO: try out more parameters for rational quadratic
+    print(cv_scores)
 
-    print(np.array(cv_scores))
-
-    # The function retrieving optimal LR parameters
-    #y_pred = modeling_and_prediction(X_train, y_train, X_test)
-    X_train, y_train, X_test = data_loading(random_state=random_state)
-
-    gpr = GaussianProcessRegressor(kernel=DotProduct(sigma_0=0.1), alpha = 5e-8) 
-
+    gpr = GaussianProcessRegressor(kernel=Matern(length_scale=0.3, nu=0.3), alpha = 5e-9)
     gpr.fit(X_train, y_train)
     y_pred = gpr.predict(X_test)
+
     # Save results in the required format
     dt = pd.DataFrame(y_pred) 
     dt.columns = ['price_CHF']
     dt.to_csv('./task 2/results.csv', index=False)
+
     print("\nResults file successfully generated!")
 
