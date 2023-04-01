886b382b74500da789991b9a8e8cba029e89e002
Marius Tresoldi <tremared@gmail.com>
2023-04-01 20:25:56 +0200

    implemented Gaussian Process as estimator in Iterative Imputer, leading to significant performance increase.

--- task 2/main.py
+++ task 2/main.py
@@ -13,93 +13,51 @@ from sklearn.gaussian_process.kernels import DotProduct, RBF, Matern, RationalQu
 from sklearn.metrics import r2_score
 from sklearn.model_selection import cross_val_score
 
+# define free parameters of the Gaussian Process
 
-def data_loading():
-    """
-    This function loads the training and test data, preprocesses it, removes the NaN values and interpolates the missing 
-    data using imputation
 
-    Parameters
-    ----------
-    Returns
-    ----------
-    X_train: matrix of floats, training input with features
-    y_train: array of floats, training output with labels
-    X_test: matrix of floats: dim = (100, ?), test input with features
-    """
+if __name__ == "__main__":
+
+    # TODO: derive motivation for best_kernel being Matern
 
     # Load training data
-    train_0 = pd.read_csv("./task 2/train.csv")
+    train = pd.read_csv("./task 2/train.csv")
 
     # Load test data
-    test_0 = pd.read_csv("./task 2/test.csv")
+    test = pd.read_csv("./task 2/test.csv")
 
     # encode categorical variable "season" with integer values according to their natural order relation provided by temperature
     seasons = ['winter', 'spring', 'autumn', 'summer']
     temperatures = [1, 2, 3, 4] 
 
-    train = train_0.replace(to_replace=seasons, value=temperatures)
-    # non-imputed, i.e. original, test data matrix
-    X_test_orig = test_0.replace(to_replace=seasons, value=temperatures)
+    train = train.replace(to_replace=seasons, value=temperatures)
+    test = test.replace(to_replace=seasons, value=temperatures)
 
-    # drop records with missing labels for regression training
-    train_reg = train.dropna(subset=['price_CHF'])
-    y_train = train_reg['price_CHF']
-    # non-imputed, i.e. original, training data matrix
-    X_train_orig = train_reg.drop(['price_CHF'], axis=1)
-
-    # Use entire feature data set for multivariate imputation training
-    train_imp = train.drop(['price_CHF'], axis=1)
-    imp = IterativeImputer(max_iter=20)
-    imp.fit(train_imp)
-
-    # transform features
-    X_train = imp.transform(X_train_orig)
-    X_test = imp.transform(X_test_orig)
-
-    assert (X_train.shape[1] == X_test.shape[1]) and (X_train.shape[0] == y_train.shape[0]) and (X_test.shape[0] == 100), "Invalid data shape"
-    return X_train, y_train, X_test
-
-
-if __name__ == "__main__":
-
-    X_train, y_train, X_test = data_loading()
-
-    kernels = [DotProduct(sigma_0=1),
-               RBF(length_scale=0.1, length_scale_bounds=(1e-06, 100000.0)),
-               Matern(length_scale=0.3, nu=0.3),
-               RationalQuadratic(length_scale=1, alpha=1)]
+    best_kernel = Matern(length_scale=0.3, nu=0.3)
+    alpha = 1e-5
+    seed = 3
+    restarts = 2
     
-    # define free parameters of the Gaussian Process
-    alpha = 1e-6
-    seed = 5
-    restarts = 5
+    imp_train = IterativeImputer(estimator=GaussianProcessRegressor(kernel=best_kernel, alpha=alpha, n_restarts_optimizer=restarts, random_state=seed), max_iter=10)
+    imp_test = IterativeImputer(estimator=GaussianProcessRegressor(kernel=best_kernel, alpha=alpha, n_restarts_optimizer=restarts, random_state=seed), max_iter=10)
+    
+    print("\ntraining training-data imputer\n") 
+    train_imputed = imp_train.fit_transform(train)
+    print("\ntraining test-data imputer\n")
+    test_imputed = imp_train.fit_transform(test)
+   
+    X_train = np.delete(train_imputed,2, axis=1)
+    y_train = train_imputed[:,2]
 
-    # initialize dataframe that holds cv score for each candidate kernel
-    cv_scores = pd.DataFrame(columns=["CV_score"], index=[str(k) for k in kernels])
-
-    # initialize best score and best kernel
-    best_score = 0
-    best_kernel = None 
-
-    for kernel in kernels:
-        print("\nUsing kernel ", str(kernel), "\n")
-        gpr = GaussianProcessRegressor(kernel=kernel, alpha=alpha, n_restarts_optimizer=restarts, random_state=seed)
-        score = np.mean(cross_val_score(gpr, X_train, y_train, cv=10, scoring="r2"))
-        cv_scores.loc[str(kernel), "CV_score"] = score
-
-        if score > best_score:
-            best_score = score
-            best_kernel = kernel
-
-    print("\nThe kernels have the following R2-CV-scores:\n")
-    print(cv_scores)
-    print("\nBest kernel is ", str(best_kernel), " and it will be used for training and generating the results.")
-
-    # fully train the model with the best kernel
     gpr = GaussianProcessRegressor(kernel=best_kernel, alpha=alpha, n_restarts_optimizer=restarts, random_state=seed)
+    print("\n cross-validating predictor\n")
+    score = np.mean(cross_val_score(gpr, X_train, y_train, cv=10, scoring="r2"))
+    print("\nR2-CV-scores:\n")
+    print(score)
+
+    print("\n training final predictor\n")
     gpr.fit(X_train, y_train)
-    y_pred = gpr.predict(X_test)
+    y_pred = gpr.predict(test_imputed)
 
     # Save results in the required format
     dt = pd.DataFrame(y_pred)
