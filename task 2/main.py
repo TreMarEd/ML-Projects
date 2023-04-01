367e0d1bab9eacb1082f4f2f1380cdfcd390a269
Marius Tresoldi <tremared@gmail.com>
2023-04-01 13:10:16 +0200

    inclusion of new kernels

--- task 2/main.py
+++ task 2/main.py
@@ -1,5 +1,7 @@
 '''
-TODO: describe the task
+This task is the second project of the 2023 intro to ML course at ETH. It consists of using Gaussian Processes to predict Swiss 
+(log-)energy prices based on the energy prices of other European countries and the season. The training data has high nullity
+such that an imputation strategy needs to be implemented during preprocessing.
 '''
 
 import numpy as np
@@ -7,12 +9,14 @@ import pandas as pd
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import DotProduct, RBF, Matern, RationalQuadratic
+from sklearn.gaussian_process.kernels import DotProduct, RBF, Matern, RationalQuadratic, Exponentiation
 from sklearn.metrics import r2_score
 from sklearn.model_selection import cross_val_score
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
+
+# TODO: general cosmetics (do this at the end)
 
 def data_loading():
     """
@@ -28,38 +32,31 @@ def data_loading():
     X_test: matrix of floats: dim = (100, ?), test input with features
     """
 
-    # TODO: visually check the correctness of all these transforamtions
     # Load training data
-    train_df = pd.read_csv("./task 2/train.csv")
-    
+    train_0 = pd.read_csv("./task 2/train.csv")
+
     # Load test data
-    test_df = pd.read_csv("./task 2/test.csv")
+    test_0 = pd.read_csv("./task 2/test.csv")
 
     # replace categorical variable season with a numeric value. The seasons have a natural order relation according to their average
-    # temperatures in Europe. Energy prices are expected to be anticorrelated with the temperature of the season. Replace each season
-    # with its average temperature in Kelvin according to the following page: 
+    # temperatures in Europe. Replace each season with its average temperature in Kelvin/100 according to the following page:
     # https://weatherspark.com/y/46917/Average-Weather-in-Eu-France-Year-Round
-
     seasons = ['spring', 'summer', 'autumn', 'winter']
-    temperatures = [282.0, 289.8, 285.4, 278.2] 
+    temperatures = [282.0, 289.8, 285.4, 278.2]
 
-    train_df = train_df.replace(to_replace=seasons, value=temperatures)
-    #original, non-imputed test data matrix 
-    X_test_orig = test_df.replace(to_replace=seasons, value=temperatures)
-
-    # visual inspection of the data shows that no column has significantly different nullity compared to the others except
-    # for season, which is always present. Hence, no column should be dropped completely. Visual inspection also shows that 
-    # there are almost no records with complete information, such that discarding missing rows is not feasible
+    train = train_0.replace(to_replace=seasons, value=temperatures)
+    # non-imputed, i.e. original, test data matrix
+    X_test_orig = test_0.replace(to_replace=seasons, value=temperatures)
 
     # drop records with missing labels for regression training
-    train_reg = train_df.dropna(subset=['price_CHF'])
+    train_reg = train.dropna(subset=['price_CHF'])
     y_train = train_reg['price_CHF']
-    # original, non-imputed training data matrix
+    # non-imputed, i.e. original, training data matrix
     X_train_orig = train_reg.drop(['price_CHF'], axis=1)
 
     # Use entire feature data set for multivariate imputation training
-    train_imp = train_df.drop(['price_CHF'], axis=1)
-    imp = IterativeImputer(max_iter=10)
+    train_imp = train.drop(['price_CHF'], axis=1)
+    imp = IterativeImputer(max_iter=20)
     imp.fit(train_imp)
 
     # transform regression features
@@ -74,32 +71,49 @@ if __name__ == "__main__":
 
     X_train, y_train, X_test = data_loading()
 
+    # TODO: try out composite kernels
     kernels = [DotProduct(sigma_0=0.1),
-               RBF(length_scale=0.1, length_scale_bounds=(1e-06, 100000.0)), 
-               Matern(length_scale=0.2, nu=0.2), Matern(length_scale=0.3, nu=0.3), Matern(length_scale=0.4, nu=0.4), Matern(length_scale=0.5, nu=0.5),
-               RationalQuadratic(length_scale=1, alpha=1)]
+               RBF(length_scale=0.1, length_scale_bounds=(1e-06, 100000.0)),
+               Matern(length_scale=0.3, nu=0.3),
+               RationalQuadratic(length_scale=1, alpha=1),
+               Exponentiation(RationalQuadratic(length_scale=0.1), exponent=2),
+               Exponentiation(Matern(length_scale=1, nu=1), exponent=2),
+               Exponentiation(RationalQuadratic(length_scale=0.1), exponent=3),
+               Exponentiation(Matern(length_scale=1, nu=1), exponent=3)]
     
+    # define open parameters of the Gaussian Process
+    alpha = 5e-8
+    random_state = 5
+    n_restarts_optimizer = 5
+
+    # initialize dataframe that holds cv score for each tried out kernel
     cv_scores = pd.DataFrame(columns=["CV_score"], index=[str(k) for k in kernels])
+
+    # initialize best score and best kernel
+    best_score = 0
+    best_kernel = None 
 
     for kernel in kernels:
         print("\nUsing kernel ", str(kernel), "\n")
-        gpr = GaussianProcessRegressor(kernel=kernel, alpha = 5e-9)
-        cv_scores.loc[str(kernel), "CV_score"] = np.mean(cross_val_score(gpr, X_train, y_train, cv=10, scoring="r2"))
+        gpr = GaussianProcessRegressor(kernel=kernel, alpha=alpha, n_restarts_optimizer=n_restarts_optimizer, random_state=random_state)
 
-    # dot product always with bad performance, RBF with terrbible performance for all parametes,
-    # matern and rational quadratic are competeteive, polynomial does not exist in the module. 
-    # Choose matern(0.3, 0.3) in the following
-    # TODO: try out more parameters for rational quadratic
+        score = np.mean(cross_val_score(gpr, X_train, y_train, cv=10, scoring="r2"))
+        cv_scores.loc[str(kernel), "CV_score"] = score
+
+        if score > best_score:
+            best_score = score
+            best_kernel = kernel
+
+    print("\nThe kernels have the following R2-CV-scores:\n")
     print(cv_scores)
+    print("\nBest kernel is ", str(best_kernel), " and it will be used for training and generating the results.")
 
-    gpr = GaussianProcessRegressor(kernel=Matern(length_scale=0.3, nu=0.3), alpha = 5e-9)
+    gpr = GaussianProcessRegressor(kernel=best_kernel, alpha=alpha, n_restarts_optimizer=n_restarts_optimizer, random_state=random_state)
     gpr.fit(X_train, y_train)
     y_pred = gpr.predict(X_test)
 
     # Save results in the required format
-    dt = pd.DataFrame(y_pred) 
+    dt = pd.DataFrame(y_pred)
     dt.columns = ['price_CHF']
     dt.to_csv('./task 2/results.csv', index=False)
-
     print("\nResults file successfully generated!")
-
