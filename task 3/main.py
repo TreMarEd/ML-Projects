48a8e9de8d3844f20be2d3e2bc8eb055699640ba
Marius Tresoldi <tremared@gmail.com>
2023-05-02 23:18:15 +0200

    disabled data augmentation

--- task 3/main.py
+++ task 3/main.py
@@ -19,14 +19,19 @@ import time
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+#alexnet embedding size
 embedding_size = 9216
 learning_rate = 1e-4
-batches_size = 16
-num_neurons1 = 1028
-num_neurons2 = 1028
-num_neurons3 = 512
-num_epochs = 20
-num_data = 30000
+#mini batch size
+batches_size = 32
+#number of neurons of layer i
+num_neurons1 = 512
+num_neurons2 = 256
+num_neurons3 = 128
+#training epochs
+num_epochs = 50
+num_data = 60000
+#dropout rate
 do = 0
 
 #num_data = 60000
@@ -36,7 +41,6 @@ def generate_embeddings():
     Transform, resize and normalize the images and then use a pretrained model to extract 
     the embeddings.
     """
-    #train_transforms = transforms.Compose([transforms.ToTensor(), ResNet50_Weights.IMAGENET1K_V2.transforms()])
     train_transforms = transforms.Compose([transforms.ToTensor(), AlexNet_Weights.IMAGENET1K_V1.transforms()])
     train_dataset = datasets.ImageFolder(root="./task 3/dataset/", transform=train_transforms)
 
@@ -44,8 +48,6 @@ def generate_embeddings():
     train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=False, pin_memory=True, num_workers=0)
 
     model = alexnet(weights=AlexNet_Weights.IMAGENET1K_V1).features
-    #model = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2) 
-    #model = torch.nn.Sequential(*(list(model.children())[:-1]))
     model.eval()
     
     num_images = len(train_dataset)
@@ -109,9 +111,9 @@ def get_data(file, train=True):
         y.append(1)
 
         # Generating negative samples (data augmentation)
-        if train:
-            X.append(np.hstack([emb[0], emb[2], emb[1]]))
-            y.append(0)
+        #if train:
+            #X.append(np.hstack([emb[0], emb[2], emb[1]]))
+            #y.append(0)
 
     X = np.vstack(X)
     y = np.hstack(y)
@@ -150,10 +152,8 @@ class Net(nn.Module):
         super().__init__()
         self.fc1 = nn.Linear(embedding_size, num_neurons1)
         #self.do1 = nn.Dropout(do)
-
         self.fc2 = nn.Linear(num_neurons1, num_neurons2)
         #self.do2 = nn.Dropout(do)
-
         self.fc3 = nn.Linear(num_neurons2, num_neurons3)
         #self.do3 = nn.Dropout(do)
         
@@ -175,7 +175,6 @@ class Net(nn.Module):
         A = F.relu(self.fc1(A))
         B = F.relu(self.fc1(B))
         C = F.relu(self.fc1(C))
-
         #A = self.do1(A)
         #B = self.do1(B)
         #C = self.do1(C)
@@ -183,7 +182,6 @@ class Net(nn.Module):
         A = F.relu(self.fc2(A))
         B = F.relu(self.fc2(B))
         C = F.relu(self.fc2(C))
-        
         #A = self.do2(A)
         #B = self.do2(B)
         #C = self.do2(C)  
@@ -191,7 +189,6 @@ class Net(nn.Module):
         A = self.fc3(A)
         B = self.fc3(B)
         C = self.fc3(C)
-
         #A = self.do3(A)
         #B = self.do3(B)
         #C = self.do3(C)
@@ -200,7 +197,7 @@ class Net(nn.Module):
         B = F.softmax(B, dim=1)
         C = F.softmax(C, dim=1)
 
-        # compute cross entropy on classifier distribution
+        # compute cross entropy on classifier distributions
         B = torch.log(B)
         C = torch.log(C)
         delta_AB = -torch.sum(torch.mul(A,B),dim=1)
@@ -208,13 +205,11 @@ class Net(nn.Module):
         
         # use cross entropy as dissimilarity measure
         x = delta_AC - delta_AB
-
         x = F.sigmoid(x)
-        
         return x
         
 
-def train_model(X_train, y_train, X_vali, y_vali):
+def train_model(train_loader):
     """
     The training procedure of the model; it accepts the training data, defines the model 
     and then trains it.
@@ -230,7 +225,8 @@ def train_model(X_train, y_train, X_vali, y_vali):
     criterion = torch.nn.BCELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     epochs = num_epochs
-    num_batches = int(y_train.size(dim=0)/batches_size)+1
+    #num_batches = int(y_train.size(dim=0)/batches_size)+1
+
     vali_losses = []
     train_losses = []
     vali_accuracies = []
@@ -240,12 +236,15 @@ def train_model(X_train, y_train, X_vali, y_vali):
         print(f'--------EPOCH {epoch}--------')
         tic = time.perf_counter()
 
-        for i in range(num_batches):
-            start= i * batches_size
-            end = (i + 1) * batches_size
+        #for i in range(num_batches):
+        for [X_batch, y_batch] in train_loader:
+            #start = i * batches_size
+            #end = (i + 1) * batches_size
             optimizer.zero_grad()
-            y_pred = model.forward(X_train[start:end, :])
-            loss = criterion(y_pred, y_train[start:end])
+            #y_pred = model.forward(X_train[start:end, :])
+            #loss = criterion(y_pred, y_train[start:end])
+            y_pred = model.forward(X_batch)
+            loss = criterion(y_pred, y_batch)
             loss.backward()
             optimizer.step()
         
@@ -273,7 +272,7 @@ def train_model(X_train, y_train, X_vali, y_vali):
             print(f"vali accuracy    {100*vali_accuracy:.2f}%")  
 
             toc = time.perf_counter()
-            print(f"elapsed minutes: {(toc-tic)/60:.2f}")
+            print(f"elapsed minutes: {(toc-tic)/60:.1f}")
 
         model.train()
     
@@ -283,7 +282,7 @@ def train_model(X_train, y_train, X_vali, y_vali):
     axes = plt.plot([i for i in range(epochs)], [item.item() for item in vali_accuracies], 'g--', label="vali accuracy")
 
     plt.xlabel("epoch")
-    plt.ylabel("Loss")
+    plt.ylabel("loss")
     plt.legend(loc="upper right")
     plt.grid()
     plt.savefig(f"./task 3/learning_curve_lr{learning_rate}_N{num_neurons1/1000}_{num_neurons2/1000}_{num_neurons3/1000}_b{batches_size}_partial_{int(num_data/1000)}k_do{do}.png")
@@ -331,25 +330,30 @@ if __name__ == '__main__':
     X_test, _ = get_data(TEST_TRIPLETS, train=False)
 
     print("\npreparing train vali split")
-    np.random.seed(2)
+    #np.random.seed(2)
     p = 0.8
-    mask_train = [bool(np.random.binomial(1, p)) for i in range(np.shape(X)[0])]
-    mask_vali = [not i for i in mask_train]
+    X_train, X_vali, y_train, y_vali = train_test_split(X, y, train_size=p, random_state=42)
+    #mask_train = [bool(np.random.binomial(1, p)) for i in range(np.shape(X)[0])]
+    #mask_vali = [not i for i in mask_train]
 
-    X_train = X[mask_train, :]
-    y_train = y[mask_train]
+    #X_train = X[mask_train, :]
+    #y_train = y[mask_train]
 
-    X_vali = X[mask_vali, :]
-    y_vali = y[mask_vali]
+    #X_vali = X[mask_vali, :]
+    #y_vali = y[mask_vali]
     
     # Create data loaders for the training and testing data
-    print("\npreparing test loader")
+    print("\npreparing loaders")
+    train_loader = create_loader_from_np(X_train, y_train, train = True, batch_size=batches_size)
     test_loader = create_loader_from_np(X_test, train = False, batch_size=1000, shuffle=False)
 
-    # define a model and train it
     print("\nstart training:\n")
-    model = train_model(X_train=torch.from_numpy(X_train).type(torch.float), y_train=torch.from_numpy(y_train).type(torch.float),
-                        X_vali=torch.from_numpy(X_vali).type(torch.float), y_vali=torch.from_numpy(y_vali).type(torch.float))
+    X_train=torch.from_numpy(X_train).type(torch.float)
+    y_train=torch.from_numpy(y_train).type(torch.float)
+    X_vali=torch.from_numpy(X_vali).type(torch.float)
+    y_vali=torch.from_numpy(y_vali).type(torch.float)
+
+    model = train_model(train_loader)
     
     # test the model on the test data
     test_model(model, test_loader)
