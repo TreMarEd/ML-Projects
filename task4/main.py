1539b49f4fe571ed9c5a018fec5fc24d150d3b05
Marius Tresoldi <tremared@gmail.com>
2023-05-14 12:39:30 +0200

    better modularization, introduction of polynomial trafo for AE features, AE now trained on pretrain and test data

--- task4/main.py
+++ task4/main.py
@@ -7,11 +7,14 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import Ridge, Lasso
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 from sklearn.model_selection import cross_val_score
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import DotProduct, RBF, Matern, RationalQuadratic
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.decomposition import PCA
 from matplotlib import pyplot as plt
 import time
 
@@ -42,15 +45,15 @@ class LumoNet(nn.Module):
     """
     The model class, which defines our feature extractor used in pretraining.
     """
-    def __init__(self):
+    def __init__(self, dim):
         """
         The constructor of the model.
         """
         super().__init__()
         self.fc1 = nn.Linear(1000, 700)
         self.fc2 = nn.Linear(700, 350)
-        self.fc3 = nn.Linear(350, 100)
-        self.fc4 = nn.Linear(100, 1)
+        self.fc3 = nn.Linear(350, dim)
+        self.fc4 = nn.Linear(dim, 1)
 
 
     def forward(self, x):
@@ -70,7 +73,7 @@ class LumoNet(nn.Module):
 
 
 class AE(nn.Module):
-    def __init__(self):
+    def __init__(self, dim):
         super().__init__()
          
         self.encoder = nn.Sequential(
@@ -80,10 +83,10 @@ class AE(nn.Module):
             nn.ReLU(),
             nn.Linear(350, 150),
             nn.ReLU(),
-            nn.Linear(150, 60),)
+            nn.Linear(150, dim),)
          
         self.decoder = torch.nn.Sequential(
-            torch.nn.Linear(60, 150),
+            torch.nn.Linear(dim, 150),
             torch.nn.ReLU(),
             torch.nn.Linear(150, 350),
             torch.nn.ReLU(),
@@ -213,16 +216,76 @@ def make_pretraining_class(feature_extractors):
         
     return PretrainedFeatures
 
-if __name__ == '__main__':
 
-    torch.manual_seed(1)
-    alphas = np.linspace(0,100,2000)
+def train_linear(model, X, Y):
+
+    if model in ["ridge", "lasso"]:
+
+        RMSEs = []
+        alphas = np.linspace(0,200,1000)
+        for l in alphas:
+
+            if model == "ridge":
+                reg = Ridge(alpha=l)
+
+            elif model == "lasso":
+                reg = Lasso(alpha=l)
+
+            cv_scores = cross_val_score(reg, X, Y, cv=20, scoring="neg_root_mean_squared_error")
+            RMSE = -np.mean(cv_scores)
+            RMSEs.append(RMSE)
+
+        j = RMSEs.index(min(RMSEs))
+
+        if model == "ridge":
+                reg = Ridge(alpha=alphas[j])
+
+        elif model == "lasso":
+                reg = Lasso(alpha=alphas[j])
+
+        reg.fit(X, Y)
+
+        return RMSEs[j], reg
+    
+    elif model == "GPR":
+        kernels = [Matern(length_scale=0.3, nu=0.3, length_scale_bounds=(1e-08, 100000.0)), 
+                   RationalQuadratic(length_scale=1.0, alpha=1.5, length_scale_bounds=(1e-08, 100000.0))]
+
+        best_kernel = None
+        best_score = 100000
+
+        for kernel in kernels:
+            gpr = GaussianProcessRegressor(kernel=kernel, alpha=1e-8, n_restarts_optimizer=3, random_state=5)
+            score = -np.mean(cross_val_score(gpr, X, Y, cv=20, scoring="neg_root_mean_squared_error"))
+
+            if score < best_score:
+                best_kernel = kernel
+                best_score = score
+        
+        gpr = GaussianProcessRegressor(kernel=best_kernel, alpha=1e-8, n_restarts_optimizer=3, random_state=5)
+        gpr.fit(X, Y)
+
+        return best_score, gpr
+
+
+if __name__ == '__main__':
+    
+    seed = 4
+    dim_lumo = 60
+    dim_ae = 20
+    dim_PCA = 100
+    epochs_lumo = 8
+    epochs_ae = 8
+    lr_lumo = 5e-5
+    lr_ae = 1e-4
+
+    torch.manual_seed(seed)
 
     # Load data
     X_pretrain, Y_pretrain, X_train, Y_train, X_test = load_data()
 
-    lumo_net = train_nn(LumoNet(), X_pretrain, Y_pretrain, 2, 32, 5e-5, vali=True)
-    ae = train_nn(AE(), X_pretrain, X_pretrain, 2, 32, 1e-4, vali=True)
+    lumo_net = train_nn(LumoNet(dim=dim_lumo), X_pretrain, Y_pretrain, epochs_lumo, 32, lr_lumo, vali=False)
+    ae = train_nn(AE(dim=dim_ae), np.vstack((X_pretrain, X_test)), np.vstack((X_pretrain, X_test)), epochs_ae, 32, lr_ae, vali=False)
 
     lumo_func = make_nn_feature_func(lumo_net, AE=False)
     encoder_func = make_nn_feature_func(ae, AE=True)
@@ -232,55 +295,68 @@ if __name__ == '__main__':
     lumo_emb = PretrainedFeatureClass(feature_extractor="lumo")
     encoder = PretrainedFeatureClass(feature_extractor="ae")
 
+    # CREATE LUMO FEATURES
     X_tr_lumo = lumo_emb.transform(X_train)
     X_te_lumo = lumo_emb.transform(X_test.to_numpy())
     
     scaler1 = StandardScaler()
-    X_te_lumo_s = scaler1.fit_transform(X_te_lumo)
-    X_tr_lumo_s = scaler1.transform(X_tr_lumo)
+    X_te_lumo = scaler1.fit_transform(X_te_lumo)
+    X_tr_lumo = scaler1.transform(X_tr_lumo)
 
+    # CREATE AE FEATURES
     X_tr_ae = encoder.transform(X_train)
     X_te_ae = encoder.transform(X_test.to_numpy())
 
+    scaler2 = StandardScaler()
+    X_te_ae = scaler2.fit_transform(X_te_ae)
+    X_tr_ae = scaler2.transform(X_tr_ae)
+
+    # CREATE COMBINED LUMO + AE FEATURES
     X_tr_comb = np.hstack((X_tr_lumo, X_tr_ae))
     X_te_comb = np.hstack((X_te_lumo, X_te_ae))
 
-    scaler2 = StandardScaler()
-    X_te_comb_s = scaler2.fit_transform(X_te_comb)
-    X_tr_comb_s = scaler2.transform(X_tr_comb)
+    # CREATE POLY AE FEATURES 
+    poly = PolynomialFeatures(2)
+    X_tr_ae_poly = poly.fit_transform(X_tr_ae)
+    X_te_ae_poly = poly.fit_transform(X_te_ae)
 
-    RMSEs = []
+    scaler3 = StandardScaler()
+    X_te_ae_poly = scaler3.fit_transform(X_te_ae_poly)
+    X_tr_ae_poly= scaler3.transform(X_tr_ae_poly)
 
-    print("\ntraining ridge regression wo AE\n")
-    for l in alphas:
-        ridge_model = Ridge(alpha=l)
-        cv_scores = cross_val_score(ridge_model, X_tr_lumo_s, Y_train, cv=20, scoring="neg_root_mean_squared_error")
-        RMSE = -np.mean(cv_scores)
-        RMSEs.append(RMSE)
+    # CREATE COMBINED LUMO + POLY AE FEATURES
+    X_tr_comb_poly = np.hstack((X_tr_lumo, X_tr_ae_poly))
+    X_te_comb_poly = np.hstack((X_te_lumo, X_te_ae_poly))
 
-    j = RMSEs.index(min(RMSEs))
-    print(f"\nbest RMSE and alpha  {RMSEs[j]}, {alphas[j]}")
+    # CREATE PCAed COMBINED LUMP + POLY AE FEATURES
+    pca = PCA(n_components=dim_PCA)
+    X_te_comb_poly_pca = pca.fit_transform(X_te_comb_poly)
+    X_tr_comb_poly_pca = pca.transform(X_tr_comb_poly)
 
-    RMSEs = []
 
-    print("\ntraining ridge regression w AE\n")
-    for l in alphas:
-        ridge_model = Ridge(alpha=l)
-        cv_scores = cross_val_score(ridge_model, X_tr_comb_s, Y_train, cv=20, scoring="neg_root_mean_squared_error")
-        RMSE = -np.mean(cv_scores)
-        RMSEs.append(RMSE)
+    models = {"ridge": None, "GPR": None}
 
-    j = RMSEs.index(min(RMSEs))
-    print(f"\nbest RMSE and alpha  {RMSEs[j]}, {alphas[j]}")
+    features = {"lumo": X_tr_lumo, "lumo+AE": X_tr_comb, "AE": X_tr_ae, "poly AE": X_tr_ae_poly, 
+                "lumo+polyAE": X_tr_comb_poly, "PCA(lump+polyAE)": X_tr_comb_poly_pca}
     
-    model = Ridge(alpha=alphas[j])
+    scores = pd.DataFrame(index=list(features.keys()), columns=models)
+    regs = pd.DataFrame(index=list(features.keys()), columns=models)
 
-    model = model.fit(X_tr_comb_s, Y_train)
+    for model in models.keys():
+        for feature in features.keys():
+            score, reg = train_linear(model, features[feature], Y_train)
+            scores.loc[feature][model] = score
+            regs.loc[feature][model] = reg
 
-    Y_pred = model.predict(X_te_comb_s)
+    print(pca.explained_variance_ratio_, sum(pca.explained_variance_ratio_))
+    print(scores)
+    print(regs)
+    scores.to_csv(f"./task4/scores_dims{dim_lumo}-{dim_ae}-{dim_PCA}_E{epochs_lumo}-{epochs_ae}_lr{lr_lumo}-{lr_ae}_seed{seed}.csv")
 
+    Y_pred = regs.loc["lumo+AE"]["ridge"].predict(X_te_comb)
     assert Y_pred.shape == (X_test.shape[0],)
     Y_pred = pd.DataFrame({"y": Y_pred}, index=X_test.index)
     Y_pred.to_csv("./task4/results.csv", index_label="Id")
     print("Predictions saved, all done!")
+    
     
